0b1992a7036bf8ea12093956e2942ac158ff9a5f
Amardeep <amardeep129@gmail.com>
2016-02-06 14:46:36 +0530

    resolved datatable issue

--- app/assets/javascripts/main.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	//init ripple effect
-	options = {
-		 	elements 	:'.btn, ul li a',
-		 	focus 		:'.btn',
-		 	focusTimeOut: 600
-		};
-	rippleEffect = new $.RippleEffect(options);
-});
